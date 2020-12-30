2d5fd3402a7e90c63f27c77179438ba632cb636d
goreleaserbot <goreleaser@bitmaelum.com>
2020-12-30 20:14:00 +0100

    Brew formula update for bitmaelum-suite version v0.0.19

--- bitmaelum-suite.rb
+++ bitmaelum-suite.rb
@@ -2,21 +2,21 @@
 class BitmaelumSuite < Formula
   desc "BitMaelum is an end-to-end email alternative"
   homepage "https://bitmaelum.com/"
-  version "0.0.11"
+  version "0.0.19"
   license "MIT"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bitmaelum/bitmaelum-suite/releases/download/v0.0.11/bitmaelum-suite_0.0.11_darwin_x86_64.tar.gz"
-    sha256 "e0d143e0c0fb6ef49118874cefa9b6fb2488ec56b4b3f7546bee06803cd6f819"
+    url "https://github.com/bitmaelum/bitmaelum-suite/releases/download/v0.0.19/bitmaelum-suite_0.0.19_darwin_x86_64.tar.gz"
+    sha256 "b6cb65a2d8fdaa146c3fdb1188ba3537864e6a7c5d62140a6d59cdfa6c12facf"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/bitmaelum/bitmaelum-suite/releases/download/v0.0.11/bitmaelum-suite_0.0.11_linux_x86_64.tar.gz"
-    sha256 "f644f4f802fb76ac04aaca0a5bebf654c16cc311db7b9303d5de12f0c0f138ad"
+    url "https://github.com/bitmaelum/bitmaelum-suite/releases/download/v0.0.19/bitmaelum-suite_0.0.19_linux_x86_64.tar.gz"
+    sha256 "efddb1334666e47f2ca48acf693ff6143d103394c4bb0396185c7abacf4fddf0"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/bitmaelum/bitmaelum-suite/releases/download/v0.0.11/bitmaelum-suite_0.0.11_linux_arm64.tar.gz"
-    sha256 "22f03c6b466d8972a312794851df26ccb53b2d365b6d5a4288df6e1e99755155"
+    url "https://github.com/bitmaelum/bitmaelum-suite/releases/download/v0.0.19/bitmaelum-suite_0.0.19_linux_arm64.tar.gz"
+    sha256 "267149b927990c4cf26877e3f3bd81e672b1932a4dbdfe18fbcc66ab2c5e9fbd"
   end
 
   def install
